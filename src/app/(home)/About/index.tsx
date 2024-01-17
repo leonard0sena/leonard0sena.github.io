ace179027478822380622c73afe8adb62d5158ac
Leonardo Sena <leonardocsena@outlook.com>
2024-01-17 15:41:03 -0300

    :rotating_light: fix linter errors

--- src/app/(home)/About/index.tsx
+++ src/app/(home)/About/index.tsx
@@ -4,8 +4,8 @@ export function About() {
   return (
     <div className="flex w-3/6 flex-col bg-gray-200 m-10 gap-8 p-8 text-start">
       <h1 className='text-black text-xl'>About</h1>
-      <p className='text-black'>Hi!, I'm Leonardo. I'm a full-stack developer and I'm currently working at <a href="https://www.alboompro.com/">Alboom</a> as a developer.</p>
-      <p className='text-black'>I'm passionate about technology and my current stack is React, React Native, NodeJS, Typescript, Flutter, Docker, AWS, and more. My focus is on the backend, but I also like to work with the frontend.</p>
+      <p className='text-black'>Hi!, I&apos;m Leonardo. I&apos;m a full-stack developer and I&apos;m currently working at <a href="https://www.alboompro.com/">Alboom</a> as a developer.</p>
+      <p className='text-black'>I&apos;m passionate about technology and my current stack is React, React Native, NodeJS, Typescript, Flutter, Docker, AWS, and more. My focus is on the backend, but I also like to work with the frontend.</p>
       <p className='text-black'> My hobbies are car mechanics, games, and programming.</p>
     </div>
   )
