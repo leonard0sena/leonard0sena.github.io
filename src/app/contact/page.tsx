02370d26394636c0c166a670d0a2ff4c916ec7fa
Leonardo Sena <leonardocsena@outlook.com>
2024-01-31 12:23:33 -0300

    :sparkles: create contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div className="flex flex-col border-white border-2 m-10 gap-8 p-8 text-start">
+      <h1 className='text-white text-xl'>Contact me</h1>
+      <p className='text-white'>Discord: @mastersorcerer</p>
+      <p className='text-white'>Github: <a className="underline" href="https://github.com/leonard0sena" target="_blank">leonard0sena</a></p>
+    </div>
+  )
+}
